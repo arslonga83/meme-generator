73380e3a731b64c6675a75f54ed91221381eb69f
arslonga83 <jlwood83@gmail.com>
2022-10-12 21:22:51 -0700

    update header styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,11 +2,11 @@ import logo from '../assets/Troll Face.png'
 
 function Header () {
   return (
-    <div className='header'>
+    <header className='header'>
       <img src={logo} className='logo'/>
       <h1 className='title'>Meme Generator</h1>
       <h3 className='subtitle'>React Course - Project 3</h3>
-    </div>
+    </header>
   )
 }
 
