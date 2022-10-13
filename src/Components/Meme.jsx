6c4fc9ea3f9432ba078e683d853aa6f4449f1630
arslonga83 <jlwood83@gmail.com>
2022-10-13 12:11:25 -0700

    add data file. add onclick for button

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,11 +1,22 @@
+import file from '../assets/memesData';
+
 function Meme () {
+
+  const getRandomMeme = () => {
+    let num = Math.floor(Math.random() * 100) + 1;
+    console.log(file.data.memes[num].url);
+  }
+
   return (
     <main>
-      <form className='form'>
+      <div className='form'>
           <input type='text' placeholder='top text' className='input'/>
           <input type='text' placeholder='bottom text' className='input'/>
-          <button className='button'>Get a new meme image</button>
-      </form>
+          <button 
+            className='button'
+            onClick={getRandomMeme}
+            >Get a new meme image</button>
+      </div>
     </main>
   )
 }
